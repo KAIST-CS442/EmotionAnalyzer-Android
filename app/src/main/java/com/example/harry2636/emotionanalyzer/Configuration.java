a1820cba82a66c87d9d68e8ce0d7532a147a6a5b
harry2636 <harry2636@naver.com>
2017-06-16 01:31:05 +0900

    Change configuration.

--- app/src/main/java/com/example/harry2636/emotionanalyzer/Configuration.java
+++ app/src/main/java/com/example/harry2636/emotionanalyzer/Configuration.java
@@ -6,7 +6,6 @@ package com.example.harry2636.emotionanalyzer;
 
 public class Configuration {
   //TODO: fix ip address
-  public static final String SERVER_ADDRESS = "http://192.249.28.84:3000"; //This must not be localhost!!!
+  public static final String SERVER_ADDRESS = "SERVER_ADDRESS"; //This must not be localhost!!!
   public static final String API_KEY = "AIzaSyDKYGldszOioPAiSFTKrto200NbSEg1aVI";
-  public static final String VIDEO_ID = "Qu5JDOpMuEk"; // Twice TT video id
 }
